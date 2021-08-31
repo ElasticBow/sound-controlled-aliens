0b7799fd98c87d803d69b590d3dbd63981427d82
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-08-31 16:22:51 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -26,12 +26,12 @@ document.getElementById("result_confidence").innerHTML = 'Accuracy -'+ (results[
 document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 img = document.getElementById('alien1')
-img1 =document.getElementById('alien2')
-img2 =document.getElementById('alien3')
-img3 =document.getElementById('alien4') 
+img1 = document.getElementById('alien2')
+img2 = document.getElementById('alien3')
+img3 = document.getElementById('alien4') 
 if (results[0].label == "Clap") 
 {
-  img.src = aliens-01.gif;
+  img.src = 'aliens-01.gif';
   img1.src = 'aliens-02.png';
   img2.src = 'aliens-03.png';
   img3.src = 'aliens-04.png';
@@ -52,11 +52,11 @@ else if (results[0].label == "Bell")
              }
              else
               {
-                  img.src = 'aliens-01.png';
-                   img1.src = 'aliens-02.png';
-                    img2.src = 'aliens-03.png';
-                     img3.src = 'aliens-04.gif'; 
-                    }
+    img.src = 'aliens-01.png';
+    img1.src = 'aliens-02.png';
+    img2.src = 'aliens-03.png';
+    img3.src = 'aliens-04.gif'; 
+    }
 
 }
 }
